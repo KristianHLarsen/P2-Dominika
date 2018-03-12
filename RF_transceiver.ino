c318fb09237cc1ac4c5b3df61f76cf2d7d1ee63a
clausmeyer <32387509+clausmeyer@users.noreply.github.com>
2018-03-12 14:03:47 +0100

    Add files via upload
    Program der modtager og afsender data over RF

---
+++ RF_transceiver.ino
@@ -0,0 +1,36 @@
+//UNO
+int inputRF; //input Radio Frequency
+int inputSM; // input serial monitor
+void setup()
+{
+  Serial.begin(9600);
+  //Serial.print("WR_433920_3_9_0_0");
+  Serial.flush();
+}
+
+void loop()
+{
+  receive();
+  transmit();
+}
+
+void receive()
+{
+  if (Serial3.available() > 0 )
+  {
+    inputRF = Serial3.read();   //serial 3 koblet til RF modul
+    Serial.write(inputRF);      // skriver til Serial monitor
+  }
+}
+
+
+void transmit()
+{
+  if (Serial.available() > 0 )
+  {
+    inputSM = Serial.read();   // læser  hvad der bliver skrevet i Serial monitor
+    Serial3.write(inputSM);    // sender det der blev skrevet i serial monitor over RF
+    Serial.write(inputSM);     // udskriver det samme på serial monitor
+  }
+}
+
