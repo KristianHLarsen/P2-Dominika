6c78fed7926b402ee26470c1f0ba5ceeee969b1e
Kristian Larsen <khkl17@student.aau.dk>
2018-03-14 10:33:22 +0100

    wiu

--- RF_transceiver/RF_transceiver.ino
+++ RF_transceiver/RF_transceiver.ino
@@ -1,10 +1,10 @@
 //MEGA
 int inputRF; //input Radio Frequency
 int inputSM; // input serial monitor
-#define NRCHAR 15
-char inString[NRCHAR];
-char breakcaractor ='!';
+#define NRCHAR 100
 
+char inString[NRCHAR];
+char outString[NRCHAR];
 
 void setup()
 {
@@ -20,45 +20,19 @@ void setup()
 void loop()
 {
   receiveString();
-  transmit();
+  transmitString();
+  
+  //receive();
+  //transmit();
   
 }
 
-void receive()
-{
-  while (Serial3.available()){
-    }
-    inputRF = Serial3.read();   //serial 3 er koblet til RF modul
-    Serial.write(inputRF);      // skriver til Serial monitor
-    Serial.println();
-    Serial.println("Modtaget data"); 
-}
-
-
-void transmit()
-{
-  while (Serial.available() )
-  {
-    inputSM = Serial.read();   // læser  hvad der bliver skrevet i Serial monitor
-    Serial3.write(inputSM);    // sender det der blev skrevet i serial monitor over RF
-    Serial.write(inputSM);     // udskriver det samme på serial monitor
-    Serial.println();
-    Serial.println("Sendt data");
-  }
-}
 
 
 
-void receiveString(){
 
-  int modtaget;
-  Serial.println("Indtast en streng");
-  modtaget = Serial3.readBytesUntil('breakcaractor', inString, NRCHAR); //break karakter til 
-  Serial3.flush();
-  Serial.print("modtaget antal:  "); Serial.println(modtaget);
-  printString(inString); //som er &(instring[0]) = inString
 
-}
+
 
 
 
