2fd3cbece324e2f7b84f931f5fc344d4bbfb6b3e
Kristian Larsen <khkl17@student.aau.dk>
2018-03-14 15:22:17 +0100

    done

--- RF_transceiver_v2.0/RF_transceiver_v2.0.ino
+++ RF_transceiver_v2.0/RF_transceiver_v2.0.ino
@@ -1,12 +1,11 @@
-//MEGA
 int inputRF; //input Radio Frequency
 int inputSM; // input serial monitor
-#define NRCHAR 100
+#define NRCHAR 20
 #define OUTCHAR 100
-char inString[NRCHAR];
-byte outString[3] = {111,22,1};
+char instring[NRCHAR]; 
+byte outString[3] = {111, 22, 1};
 
-    String tal = String(outString[0]) + '!' + String(outString[1]) + '!' + String(outString[2]) + '/';
+//String tal = String(outString[0]) + '!' + String(outString[1]) + '!' + String(outString[2]) + '/';
 
 
 void setup()
@@ -16,14 +15,14 @@ void setup()
   //Serial.print("WR_433920_3_9_0_0");
   Serial3.begin(9600);
   Serial3.flush();
-  Serial3.setTimeout(30000);
+  Serial3.setTimeout(10000);
   Serial.flush();
-  splitUp();
+  //splitUp();
 }
 
 void loop()
 {
- // receiveString();
+  receiveString2();
   //transmit();
 
 }
@@ -40,12 +39,12 @@ void receive()
 
 void transmit()
 {
-    String tal = String(outString[0]) + '!' + String(outString[1]) + '!' + String(outString[2]) + '/';
-    Serial3.print(tal);    // sender det der blev skrevet i serial monitor over RF
-    Serial3.flush();
-    Serial.println();
-    Serial.println("Sendt data");
-    delay(1000);
+  String tal = String(outString[0]) + '!' + String(outString[1]) + '!' + String(outString[2]) + '/';
+  Serial3.print(tal);    // sender det der blev skrevet i serial monitor over RF
+  Serial3.flush();
+  Serial.println();
+  Serial.println("Sendt data");
+  delay(1000);
 }
 
 void receiveString() {
@@ -54,21 +53,41 @@ void receiveString() {
   {
     int modtaget;
     Serial.println("Indtast en streng og slut med !");
-    modtaget = Serial3.readBytesUntil(10, inString, NRCHAR); //break karakter til
+    instring = Serial3.read(); //break karakter til
     Serial3.flush();
-    Serial.print("modtaget antal:  "); Serial.println(modtaget);
-    printString(inString); //som er &(instring[0]) = inString
+    Serial.println(instring);
+   Serial.print("modtaget antal:  "); //Serial.println(modtaget);
+   // printString(inString); //som er &(instring[0]) = inString
+    splitUp();
   }
 }
 
-void splitUp()
+void receiveString2() {
+  if (Serial3.available() > 0 )
+  {
+    int modtaget;
+    Serial.println("Indtast en streng");
+    modtaget = Serial3.readBytesUntil('/', inString, NRCHAR); //break karakter = 10 =return
+    Serial3.flush();
+    Serial.print("modtaget antal:  "); Serial.println(modtaget);
+   // printString(inString, 0); //som er &(instring[0]) = inString
+   String str = String(instring);
+   Serial.print(str);
+   //splitUp(str);
+  }
+}
+
+
+
+void splitUp(String A )
 {
-  int seperatorEt = tal.indexOf('!');
-  int seperatorTo = tal.indexOf('!',seperatorEt+1);
-  int seperatorTre = tal.indexOf('/');
-  String sub1 = tal.substring(0,seperatorEt);
-  String sub2 = tal.substring(seperatorEt+1,seperatorTo);
-  String sub3 = tal.substring(seperatorTo+1, seperatorTre);
+  
+  int seperatorEt = A.indexOf('!');
+  int seperatorTo = A.indexOf('!', seperatorEt + 1);
+  int seperatorTre = A.indexOf('/');
+  String sub1 = A.substring(0, seperatorEt);
+  String sub2 = A.substring(seperatorEt + 1, seperatorTo);
+  String sub3 = A.substring(seperatorTo + 1, seperatorTre);
 
   Serial.println(sub1);
   Serial.println(sub2);
