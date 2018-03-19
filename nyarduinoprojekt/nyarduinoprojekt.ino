97c4963f628930c5dc00e837386bf13634680086
Kristian Larsen <khkl17@student.aau.dk>
2018-03-19 14:07:32 +0100

    Oprydning i Dominika

--- nyarduinoprojekt/nyarduinoprojekt.ino
+++
@@ -1,9 +0,0 @@
-void setup() {
-  // put your setup code here, to run once:
-lollern arduino kode
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-Nu sker der noget nyt llooooolll
-}
