e4e8ed12ac592f0a403d67dc446a7bf2115fda17
clausmeyer <32387509+clausmeyer@users.noreply.github.com>
2018-03-19 14:19:25 +0100

    Oprydning i modtager

--- RF_transceiver_v2.0_modtager/functions.ino
+++ RF_transceiver_v2.0_modtager/functions.ino
@@ -1,7 +1,5 @@
-
 void servoControl(int servoVal) {
-//  servoVal = analogRead(A0);
-  
+
   servoVal = map(servoVal, 0, 1023, 70, 140);     // scale it to use it with the servo (value between 0 and 180)
   myservo.write(servoVal);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
@@ -9,7 +7,6 @@ void servoControl(int servoVal) {
 
 
 void motorControl(int motorVal) {
- // motorVal = analogRead(A1);
 
   if (motorVal > 510) {
     digitalWrite(dirpin, LOW);
@@ -18,10 +15,10 @@ void motorControl(int motorVal) {
       motorSpeed = 0;
     }
   }
-if(motorVal > 490 && motorVal < 510)
-{
-  motorSpeed = 0;
-}
+  if (motorVal > 490 && motorVal < 510)
+  {
+    motorSpeed = 0;
+  }
   if (motorVal < 490) {
     digitalWrite(dirpin, HIGH);
     motorSpeed = (0.133061 * (motorVal - 490) * (-1));
@@ -29,6 +26,5 @@ if(motorVal > 490 && motorVal < 510)
       motorSpeed = 0;
     }
   }
-
   analogWrite(pwmpin, motorSpeed);
 }
