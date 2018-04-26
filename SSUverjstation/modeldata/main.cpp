9513bad0f7114538e8436a4e64a6f93f25a2e50c
Kristian Slot <Kris.1@live.dk>
2018-04-26 09:51:16 +0200

    graph

---
+++ SSUverjstation/modeldata/main.cpp
@@ -0,0 +1,12 @@
+/****************************************************************************/
+
+#include <QtWidgets/QApplication>
+#include "tablewidget.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    TableWidget w;
+    w.show();
+    return a.exec();
+}
