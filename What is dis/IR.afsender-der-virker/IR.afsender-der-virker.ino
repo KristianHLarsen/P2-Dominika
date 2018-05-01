79c60fffee1b2f3bfb5a18606baaca0dce9a4ba1
clausmeyer <32387509+clausmeyer@users.noreply.github.com>
2018-05-01 09:00:20 +0200

    .

---
+++ What is dis/IR.afsender-der-virker/IR.afsender-der-virker.ino
@@ -0,0 +1,44 @@
+int irPin = 4;
+
+const int trigPin = 6;
+const int echoPin = 7;
+// defines variables
+long duration;
+int distance;
+
+int start = 0;
+int slut = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(irPin,OUTPUT);
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int i = 0;i < 10; i++)
+{
+  digitalWrite(irPin,HIGH);
+  delayMicroseconds(13);
+  digitalWrite(irPin,LOW);
+  delayMicroseconds(13);
+}
+
+delayMicroseconds(300);    // Sikrer at modtageren altid sender ultralydssignal før afsenderen
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+//duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+//distance= duration*0.034;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+//Serial.println(distance);
+delay(200);
+}
