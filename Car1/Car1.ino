a9031770d1d33fef12db2adab489f7df3ba4f08b
Albert Sandager <albert@sandbo.dk>
2018-05-01 13:07:51 +0200

    making file for car1 work

--- Car1/Car1.ino
+++ Car1/Car1.ino
@@ -1,21 +1,22 @@
-/* SKAL LAVES:
-
-
-
-*/
+//Initialising pins for devices
 int irPin = 4;
 const int trigPin = 6;
 const int echoPin = 7;
 
 
+// defines variables
+long duration;
+int distance;
+int start = 0;
+int slut = 0;
+
+
 void setup() {
+
   setupConfig();
-  
 }
 
 void loop() {
-  pulseLoop();
 
+  triggerSignal();
 }
-
-
