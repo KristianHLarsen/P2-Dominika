352f25b0d52d125407a80406a6e5d53dfc0bfa07
clausmeyer <32387509+clausmeyer@users.noreply.github.com>
2018-05-04 08:56:09 +0200

    .

--- What is dis/RF_transceiver_v2.0_modtager/RF_transceiver_v2.0_modtager.ino
+++ What is dis/RF_transceiver_v2.0_modtager/RF_transceiver_v2.0_modtager.ino
@@ -9,15 +9,18 @@ char instring[NRCHAR];
 byte outString[3] = {111, 22, 1};
 
 int motorVal;
-int dirpin = 11;
-int pwmpin = 6;
+//int dirpin = 11;
+int INa = 7;
+int INb = 6;
+int pwmpin = 5;
 int motorSpeed; // midsteværdi 25 dog maks. 255
 int servoVal;
 
-int irPin = 4;
-const int trigPin = 5;
-const int echoPin = 7;
+int irPin = 2;
+const int trigPin = 3;
+const int echoPin = 4;
 
+long lastMillis;
 
 // defines variables
 long duration;
@@ -27,10 +30,11 @@ int slut = 0;
 
 
 
-Servo myservo;  
+Servo myservo;
 
 void setup()
 {
+  lastMillis = 0;
   Serial.begin(9600);
   Serial.setTimeout(30000);
   //Serial.print("WR_433920_3_9_0_0");
@@ -38,17 +42,23 @@ void setup()
   Serial3.flush();
   Serial3.setTimeout(10000);
   Serial.flush();
-
-  pinMode(dirpin, OUTPUT);
+  setupConfig();
+  //pinMode(dirpin, OUTPUT);
   pinMode(pwmpin, OUTPUT);
+  pinMode(INa, OUTPUT);
+  pinMode(INb,OUTPUT);
+    digitalWrite(INa,HIGH);
+    digitalWrite(INb,LOW);
   myservo.attach(9);
 }
 
 void loop()
 {
-
-  receiveString();
-
+  while (millis() - lastMillis < 200)
+  {
+    receiveString();
+  }
+  triggerSignal();
 }
 
 void receiveString() {
@@ -75,7 +85,7 @@ void splitUp(String A )
   int PWM_H_bridge = sub1.toInt();
   int PWM_Servo = sub2.toInt();
   int DIR_H_bridge = sub3.toInt();
-  
+
   Serial.println(PWM_H_bridge);
   Serial.println(PWM_Servo);
   Serial.println(DIR_H_bridge);
--- What is dis/RF_transceiver_v2.0_modtager/lib.ino
+++ What is dis/RF_transceiver_v2.0_modtager/lib.ino
@@ -1,7 +1,7 @@
 void servoControl(int servoVal) {
 //  servoVal = analogRead(A0);
   
-  servoVal = map(servoVal, 0, 1023, 75, 145);     // scale it to use it with the servo (value between 0 and 180)
+  servoVal = map(servoVal, 0, 1023, 55, 115);    // Maks 35-135 // scale it to use it with the servo (value between 0 and 180)
   myservo.write(servoVal);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
 }
@@ -11,7 +11,8 @@ void motorControl(int motorVal, int boost) {
  // motorVal = analogRead(A1);
 
   if (motorVal > 510) {
-    digitalWrite(dirpin, LOW);
+    digitalWrite(INa, LOW);
+    digitalWrite(INb, HIGH);
     motorSpeed = (0.13199 * (motorVal - 510));
     if (motorSpeed < 0) {
       motorSpeed = 0;
@@ -22,7 +23,8 @@ if(motorVal > 490 && motorVal < 510)
   motorSpeed = 0;
 }
   if (motorVal < 490) {
-    digitalWrite(dirpin, HIGH);
+    digitalWrite(INa, HIGH);
+    digitalWrite(INb, LOW);
     motorSpeed = (0.133061 * (motorVal - 490) * (-1) * boost);
     if (motorSpeed < 0) {
       motorSpeed = 0;
