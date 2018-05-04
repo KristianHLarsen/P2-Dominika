db37db0906eddd23e810a4f950282364cb80b0f2
Kristian Slot <Kris.1@live.dk>
2018-05-04 10:09:11 +0200

    RF protototokol

--- RF-platoon/RF_master/RF_master.ino
+++ RF-platoon/RF_master/RF_master.ino
@@ -6,9 +6,6 @@ int inputSM;       // input serial monitor
 #define INCHAR 20  //size of array for characters coming in
 char inString[INCHAR];
 byte outString[OUTCHAR];
-float tStart;
-float tStop;
-float tReak;
 int test;
 
 void setup  () {
@@ -32,51 +29,36 @@ void transmit()
   delay(10);
 }
 
-void receiveString2() {
+void receiveString() {
   if (Serial3.available() > 0 )
   {
-    char a = Serial3.read();
-    if (a == '=') {
-      int modtaget;
-      modtaget = Serial3.readBytesUntil('/', inString, INCHAR); //break karakter = 10 =return
-      Serial3.flush();
-      String str = String(inString);
+    int modtaget;
+    modtaget = Serial3.readBytesUntil('/', inString, INCHAR); //break karakter = 10 =return
+    Serial3.flush();
+    String str = String(inString);
+    char startChar = str.charAt(0);
+    if (startChar == '=') {
       splitUp(str, modtaget);
-      Serial.flush();
     }
-  }
-  else  {
-    //do nothing
+    Serial.flush();
   }
 }
 
+
 void splitUp(String A, int modtaget )
 {
-  int seperatorEt = A.indexOf('?');
-  String sub1 = A.substring(0, seperatorEt);
-  //test = sub1.toInt(); 
+  int startSeperator = A.indexOf('=') + 1;
+  int slutSeperator = A.indexOf('!');
+  String sub1 = A.substring(startSeperator, slutSeperator);
+  //test = sub1.toInt();
   //Serial.println("Modtaget: " + String(test));
   Serial.println("Modtaget: " + String(sub1));
 }
 
-void timer()  {
-  transmit();
-  tStart = micros(); // Time in milliseconds since start
-  receiveString2();
-  while (Serial3.available() == 0)  {
-    //No code - just busy waiting
-  }
-  tStop = micros(); // Time in milliseconds since stop
-  tReak =  tStop - tStart;
-  Serial.print("Forsinkelse: ");
-  Serial.print(tReak);  Serial.print(" micro s");
-  Serial.println();
-  Serial.println();
-}
-
 void loop()
 {
-  timer();
+  receiveString();
+  transmit();
   delay(1000);
 }
 
