7a5c3ed6da77742ad4b56192a2a072c15939cfe1
Kristian Slot <Kris.1@live.dk>
2018-05-07 09:02:10 +0200

    hej

--- ENDELIG PROJEKTMAPPE/Car1/Car1.ino
+++ ENDELIG PROJEKTMAPPE/Car1/Car1.ino
@@ -20,6 +20,7 @@ int irPin = 2;
 const int trigPin = 3;
 const int echoPin = 4;
 
+unsigned long RFmillis;
 long lastMillis;
 
 // defines variables
@@ -34,6 +35,7 @@ Servo myservo;
 
 void setup()
 {
+  RFmillis = millis();
   lastMillis = 0;
   Serial.begin(9600);
   Serial.setTimeout(30000);
@@ -58,6 +60,9 @@ void loop()
   {
     receiveString();
   }
+  if ((millis() - RFmillis) > 300)  {
+    
+  }
   triggerSignal();
 }
 
@@ -68,10 +73,9 @@ void receiveString() {
     Serial3.flush();
     String str = String(instring);
     splitUp(str);
+    RFmillis = millis();
   }
 }
-
-
 
 void splitUp(String A )
 {
@@ -81,7 +85,6 @@ void splitUp(String A )
   String sub1 = A.substring(0, seperatorEt);
   String sub2 = A.substring(seperatorEt + 1, seperatorTo);
   String sub3 = A.substring(seperatorTo + 1, seperatorTre);
-
   int PWM_H_bridge = sub1.toInt();
   int PWM_Servo = sub2.toInt();
   int DIR_H_bridge = sub3.toInt();
